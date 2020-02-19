cd72071eaa122bde11326f21c62bda0e5e9c9bff
Bruce Cota <bruce.cota@komodohealth.com>
2020-02-19 09:38:21 -0500

    adding a stored proc as a demonstration of failure

---
+++ list_stage.sql
@@ -0,0 +1,21 @@
+use schema bruce;
+
+create or replace procedure list_stage(SCHEMA_NAME VARCHAR, STAGE_NAME VARCHAR)
+returns varchar
+language javascript as
+$$
+var sql = `list @${SCHEMA_NAME}.${STAGE_NAME}`
+var stmt = snowflake.createStatement({
+    sqlText: sql,
+    binds: []
+})
+var rs = stmt.execute()
+return_list = []
+while (rs.next()) {
+      var file = rs.getColumnValue(1)
+      var size = rs.getColumnValue(2)
+      return_list.push(`${file}:${size}`)
+}
+return return_list.join("\n")
+$$
+
