13b2a7847b947f66af694c1072a397a96de22468
Bruce Cota <bruce.cota@komodohealth.com>
2020-03-10 23:44:56 -0400

    simplification of row_counts

--- row_counts.sql
+++ row_counts.sql
@@ -11,9 +11,8 @@ $$
     var rc_table = `tmp_row_counts`;
 
     sql = `CREATE OR REPLACE TEMPORARY TABLE ${rc_table} `+
-        `AS SELECT regexp_replace(file_name, '.*/', '') AS file_name, 'All' AS aggregation_key, num_rows FROM ` + 
-        `(SELECT metadata$filename AS file_name,  count(*) AS num_rows ` +
-        `FROM ${LOCATION} (file_format => ${FILE_FORMAT}) group by metadata$filename)`
+        `AS SELECT regexp_replace(metadata$filename, '.*/', '') AS file_name, 'All' AS aggregation_key, count(*) AS num_rows ` + 
+        `FROM ${LOCATION} (file_format => ${FILE_FORMAT}) group by metadata$filename`
     snowflake.createStatement({sqlText: sql, binds: []}).execute()
 
 
