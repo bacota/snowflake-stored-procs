1207f8ecfb0d4fcd66aa0236ba31676b81cd02d2
Bruce Cota <bruce.cota@komodohealth.com>
2020-03-10 23:39:02 -0400

    fixes for row_counts

--- row_counts.sql
+++ row_counts.sql
@@ -11,8 +11,9 @@ $$
     var rc_table = `tmp_row_counts`;
 
     sql = `CREATE OR REPLACE TEMPORARY TABLE ${rc_table} `+
-        `AS SELECT regexp_replace(metadata$filename, '.*/', '') AS file_name, 'All' AS aggregation_key, count(*) AS num_rows ` +
-        `FROM ${LOCATION} (file_format => ${FILE_FORMAT}) group by metadata$filename`
+        `AS SELECT regexp_replace(file_name, '.*/', '') AS file_name, 'All' AS aggregation_key, num_rows FROM ` + 
+        `(SELECT metadata$filename AS file_name,  count(*) AS num_rows ` +
+        `FROM ${LOCATION} (file_format => ${FILE_FORMAT}) group by metadata$filename)`
     snowflake.createStatement({sqlText: sql, binds: []}).execute()
 
 
@@ -25,5 +26,5 @@ $$
 $$;
 
 
-call bruce.create_row_counts('@GUARDANT.export_stage/Medical_Headers_DAT/',
-   'parquet', '@TEST_STAGE/summaries/row_counts.csv');
+--call bruce.create_row_counts('@GUARDANT.export_stage/Medical_Headers_DAT/',
+--   'parquet', '@TEST_STAGE/summaries/row_counts.csv');
